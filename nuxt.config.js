f033782d3790fd9c0111f86570ea115323842919
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-02-12 16:15:08 +0800

    Update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,11 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: 'favicon/apple-touch-icon.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: 'favicon/favicon-32x32.png' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: 'favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: 'favicon/site.webmanifest' }
     ]
   },
 
